d4ebd36f3bcb877338af08e7aba6d9d9c38e4875
unknown <isaiahadejumo@gmail.com>
2019-03-03 02:17:51 -0800

    commented MATLAB solution

--- MATLAB/ode_sys.m
+++ MATLAB/ode_sys.m
@@ -1,16 +1,17 @@
 function diffeqs=ode_sys(t, var)
-    r=var(1);
-    theta=var(2);
-    p_r=var(3);
-    p_theta=var(4);
+    r=var(1); %length of pendulum
+    theta=var(2); %angle of pendulum from vertical
+    p_r=var(3); %pendulum momentum in the r direction
+    p_theta=var(4); %pendulum momentum in the theta direction
     
-    g=9.81;
-    m=1;
-    u=2.394;
-    M=u*m;
+    g=9.81; %gravitationa1 constant
+    m=1; %pendulum mass
+    u=2.394; %mass ratio
+    M=u*m; %larger mass
     
-    diffeqs(1,1)= p_r/(M+m);
-    diffeqs(2,1)= p_theta/(m*r^2);
-    diffeqs(3,1)= (p_theta^2/(m*r^3)) - M*g + m*cos(theta);
-    diffeqs(4,1)= -m*g*r*sin(theta);
+    %differential equations obtained from hamiltonian
+    diffeqs(1,1)= p_r/(M+m); %dr/dt = dH/dp_r
+    diffeqs(2,1)= p_theta/(m*r^2); %dtheta/dt = dH/dp_theta
+    diffeqs(3,1)= (p_theta^2/(m*r^3)) - M*g + m*cos(theta); %dp_r/dt = dH/dr
+    diffeqs(4,1)= -m*g*r*sin(theta); %dp_theta/dt = dH/dtheta
 end
--- MATLAB/script.m
+++ MATLAB/script.m
@@ -1,9 +1,9 @@
+%solving differential equations
 
-%range=[1:60:100];
-range = linspace(0,20,10001);
-ICs=[2, pi/2, 0, 0];
+range = linspace(0,20,10001); %time values
+ICs=[2, pi/2, 0, 0]; %initial conditions [r, theta, p_r, p_theta]
 
 [tsol, varsol]=ode45(@ode_sys,range,ICs);
 
-%theta = 
+%plotting solutions
 polar(varsol(:,2) - pi/2, varsol(:,1));
