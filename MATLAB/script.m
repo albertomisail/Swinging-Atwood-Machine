0c34fe31d3232220d0e1c9a0a8804191cccd7515
unknown <isaiahadejumo@gmail.com>
2019-03-04 17:28:57 -0800

    First attempt at MATLAB 3D plot

--- MATLAB/script.m
+++ MATLAB/script.m
@@ -7,4 +7,11 @@ ICs=[5, pi/2, 0, 0]; %initial conditions [r, theta, p_r, p_theta]
 [tsol, varsol]=ode45(@ode_sys,range,ICs);
 
 %plotting solutions
+theta = varsol(:,2) - pi/2;
+rho = varsol(:,1);
+
+[x,y] = pol2cart(theta, rho);
+figure(1)
 polar(varsol(:,2) - pi/2, varsol(:,1));
+figure(2)
+plot(x,y);
